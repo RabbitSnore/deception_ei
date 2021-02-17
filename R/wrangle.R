9907a0a5cad38b3b37bbca6db6152f3225199452
RabbitSnore <rabbitsnore@gmail.com>
2021-02-17 18:04:03 +0100

    getting started on the wrangle

---
+++ R/wrangle.R
@@ -0,0 +1,34 @@
+#######################################################################
+
+# Wrangling
+
+#######################################################################
+
+# Load packages 
+
+packages <- c("dplyr", "tidyr", "readxl")
+
+lapply(packages, library, character.only = TRUE)
+
+# Import data
+
+raw <- read_xlsx("deception_ei_data.xlsx")
+
+# Tidying up
+
+## Change all column names to lower case
+
+colnames(raw) <- tolower(colnames(raw))
+
+## Select accuracy columns
+
+accuracy <- raw %>% 
+  select(ss, starts_with("accur_"))
+
+accuracy_long <- accuracy %>% 
+  pivot_longer(
+    cols = starts_with("accur_"),
+    names_to = "name",
+    values_to = "accuracy"
+  ) %>% 
+  extract(col = "name", into = c("sender", "veracity"), regex = "accur_(.*)_(.*)")
